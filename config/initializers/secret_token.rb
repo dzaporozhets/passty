81124db0428b208e8887dcca076a41e2e0ef698a
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-05-18 09:55:42 +0300

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Passty::Application.config.secret_key_base = 'b036226851f5dc1b4f76bef3968d2e037b58514e1e4e929389835ee014ae5b256beb06fa63aca16631b4a2cb62618994a85de5cea329e697d9579a897407ab25'

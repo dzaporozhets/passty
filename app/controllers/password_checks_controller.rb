959e07d73b396028a9c45e375823453da0204654
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2013-08-30 23:02:41 +0300

    Fix timezone issue when password check asks in loop

--- app/controllers/password_checks_controller.rb
+++ app/controllers/password_checks_controller.rb
@@ -6,7 +6,7 @@ class PasswordChecksController < ApplicationController
     if current_user.valid_password?(params[:password])
       cookies[:password_confirmed] = {
         value: current_user.encrypt(Date.today.to_s),
-        expires: 15.minutes.from_now
+        expires: 15.minutes.from_now(Time.now)
       }
 
       redirect_to params[:redirect_to]
